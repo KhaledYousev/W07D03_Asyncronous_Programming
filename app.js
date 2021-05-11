382bf156b11ff816d99856d8ab9747372f222a83
KhaledYousev <mr.khaledyusuf@gmail.com>
2021-05-12 00:32:40 +0300

    pulse 3 Done

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 const fs = require("fs");
 const axios = require("axios");
 
-app.use(express.json());
-
-
+const readFile = () => {
+  let content;
+  fs.readFile("data.txt", (err, data) => {
+    if (err) throw err;
+    content = data.toString();
+    console.log(content);
+  });
+};
+readFile();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
